de5861220a1cf9a218a15b114e1cae504029f875
opsb <oliver@opsb.co.uk>
2015-04-01 20:16:08 -0600

    wip - subscribe to initial nested hasOne records

--- test/tests/orbit_firebase/unit/firebase-listener/subscriptions-test.js
+++ test/tests/orbit_firebase/unit/firebase-listener/subscriptions-test.js
@@ -8,6 +8,7 @@ import Orbit from 'orbit/main';
 import { captureDidTransform, captureDidTransforms, op } from 'tests/test-helper';
 import { fop } from 'orbit-firebase/lib/operation-utils';
 import { Promise, all } from 'rsvp';
+import { buildOptions } from 'orbit-firebase/subscriptions/options';
 
 var schemaDefinition = {
   modelDefaults: {
@@ -16,6 +17,14 @@ var schemaDefinition = {
     }
   },
   models: {
+    star: {
+      attributes: {
+        name: {type: 'string'}
+      },
+      links: {
+        planets: {type: 'hasMany', model: 'planet', inverse: 'star'}
+      }
+    },
     planet: {
       attributes: {
         name: {type: 'string'},
@@ -25,7 +34,8 @@ var schemaDefinition = {
         moons: {type: 'hasMany', model: 'moon', inverse: 'planet'},
         races: {type: 'hasMany', model: 'race', inverse: 'planets'},
         next: {type: 'hasOne', model: 'planet', inverse: 'previous'},
-        previous: {type: 'hasOne', model: 'planet', inverse: 'next'}
+        previous: {type: 'hasOne', model: 'planet', inverse: 'next'},
+        star: {type: 'hasOne', model: 'star', inverse: 'planets'}
       }
     },
     moon: {
@@ -235,6 +245,34 @@ test("subscribe to a record when it's hasOne is replaced", function(){
 });
 
 test("subscribe to initial nested hasOne records", function(){
+  stop();
+  var sun = { id: 'star1', name: "The Sun", planets: { 'planet1': true } };
+  var jupiter = { id: 'planet1', name: 'Jupiter', star: 'star1', moons: {'moon1': true} };
+  var europa = { id: 'moon1', name: 'Europa', planet: 'planet1' };
+
+  var capture = captureDidTransforms(firebaseListener, 5, {logOperations: true});
+
+  all([
+    firebaseClient.set('star/star1', sun),
+    firebaseClient.set('planet/planet1', jupiter),
+    firebaseClient.set('moon/moon1', europa)
+
+  ])
+  .then(function(){
+    return firebaseListener.subscribeToRecord('moon', 'moon1', buildOptions({include: ['planet.star']}));
+  });
+
+  capture.then(function(){
+    start();
+    includesAll(firebaseListener.subscriptions(), [
+      "moon/moon1/name:value",
+      "moon/moon1/planet:value",
+      "moon/moon1:value",
+      "planet/planet1/classification:value",
+      "planet/planet1/name:value",
+      "planet/planet1:value"
+    ]);
+  });
 });
 
 test("subscribe to added nested hasOne records", function(){
