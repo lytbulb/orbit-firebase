61c108f2e5e14f1a3489ed2d5df27967192cb53a
opsb <oliver@opsb.co.uk>
2015-06-05 09:55:24 -0500

    Introduce Subscription base class

--- Brocfile.js
+++ Brocfile.js
@@ -28,6 +28,7 @@ var packages = [
               /orbit-firebase\/firebase-source.js/,
               /orbit-firebase\/firebase-listener.js/,
               /orbit-firebase\/transformations.js/,
+              /orbit-firebase\/subscriptions\/subscription.js/,
               /orbit-firebase\/subscriptions\/record-subscription.js/,
               /orbit-firebase\/subscriptions\/attribute-subscription.js/,
               /orbit-firebase\/subscriptions\/has-one-subscription.js/,
